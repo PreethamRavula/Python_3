b77d5e40ac61c4f222bac9eee53533c3c7ba8c5a
Sri Preetham Sai Ravula <preethamsai.ravula@gmail.com>
2021-10-23 03:20:03 -0700

    New python file Intials

---
+++ Hello_World/Initials.py
@@ -0,0 +1,9 @@
+# Sri Preetham Sai Ravula
+# Fun Fact: I am afraid of Heights
+print(" sss    pppp    sss   RRRR ")
+print("s   s   p   p  s   s  R   R")
+print("s       p   p  s      R   R")
+print(" sss    ppp     sss   RRRR ")
+print("    s   p          s  R R  ")
+print("s   s   p      s   s  R  R ")
+print(" sss    p       sss   R   R")
